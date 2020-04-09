492b52e00d9be3ea3aa69d92acc4ef525756b64d
Musa7112 <60511840+Musa7112@users.noreply.github.com>
2020-04-09 11:34:19 +0600

    Add files via upload

---
+++ week1assighment1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHPweek1</title>
+</head>
+<body style="background-color: #000000; color: #ffffff">
+     <p>"Hello World"</p>
+	 <?php echo "Hello PHP Wotld" ?>
+</body>
+</html>
